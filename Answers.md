c51fddf38adde69c9cc2e676561ba0851e2e3d74
Alexander Fukui <alexander.fukui@gmail.com>
2019-11-11 13:35:46 -0600

    done up to 5

--- Answers.md
+++ Answers.md
@@ -3,3 +3,13 @@
 1.2 Once our HashMap is ran it increases the capacity from the initial value of 8 to 24 in order to make room for additional key value pairs. This is done because our Max Load ratio of 50% was surpassed.
 
 2. The function instantiates two different HashMaps and attempts to assign two key value pairs to each one. However, the key of every key value pair is identical, thus the second assignment overwrites the first. So the console.logs will return the second assignments values => (20) , (10).
+
+3. 
+
+  1. | 22 | 88 |  |  | 4 | 15 | 28 | 17 | 31 | 10 | 59 |
+
+  2. |  | 28 | 20 | 12 |  | 5 | 15 |  | 17 |
+        | 19 |                | 33 |
+        | 10 |
+
+4. 
--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -17,11 +17,70 @@ const main = () => {
   lor.set('LadyOfLight', 'Galadriel');
   lor.set('HalfElven', 'Arwen');
   lor.set('Ent', 'Treebeard');
-  
-  // return lor.get('Hobbit');
-  // return lor.get('Maiar');
+
   return lor;
 };
 
 // console.log(main());
 
+// input: google, output: gole
+// save to hashmap, key letter itself
+// instead of replacing duplicate key, it should discard it - check if key exists, then discard if it does, save if it doesn't.
+// Keep in order - save character order alongside value?
+
+function removeDuplicates(string) {
+  const stringMap = new HashMap();
+
+  HashMap.MAX_LOAD_RATIO = 0.5;
+  HashMap.SIZE_RATIO = 3;
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    stringMap.set(string[i], i);
+  }
+
+  let newString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (i === stringMap.get(string[i])) {
+      newString += string[i];
+    }
+  }
+  return newString;
+}
+
+// console.log(removeDuplicates('google all that you think can think of'));
+
+function checkPalindrome(string) {
+  const stringMap = new HashMap();
+
+  HashMap.MAX_LOAD_RATIO = 0.5;
+  HashMap.SIZE_RATIO = 3;
+
+  //map string, set key=char, value=value+1
+  //if letters count are equal, it is palindrome - there has to be an EVEN number of occurances for each char,
+  // and only one ODD occurance if total number of letters is odd.
+  for (let i = 0; i < string.length; i++) {
+    stringMap.set(string[i], 0);
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    let counter = stringMap.get(string[i]) + 1;
+    stringMap.set(string[i], counter);
+  }
+
+  let oddCounter = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (stringMap.get(string[i]) % 2 !== 0) {
+      oddCounter += 1;
+    }
+  }
+
+  if (oddCounter > 1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(checkPalindrome('acecarr'));
+console.log(checkPalindrome('tattarrattat'));
+console.log(checkPalindrome('north'));
