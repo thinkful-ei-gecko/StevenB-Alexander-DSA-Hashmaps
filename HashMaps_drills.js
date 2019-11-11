fe228d2f026c925b13b6d9c80f74738644cd8a8c
Alexander Fukui <alexander.fukui@gmail.com>
2019-11-11 14:51:24 -0600

    working through 6

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -81,6 +81,65 @@ function checkPalindrome(string) {
   }
 }
 
-console.log(checkPalindrome('acecarr'));
-console.log(checkPalindrome('tattarrattat'));
-console.log(checkPalindrome('north'));
+// console.log(checkPalindrome('acecarr'));
+// console.log(checkPalindrome('tattarrattat'));
+// console.log(checkPalindrome('north'));
+
+function anagramGrouping(arr) {
+
+  const arrMap = new HashMap();
+
+  HashMap.MAX_LOAD_RATIO = 0.5;
+  HashMap.SIZE_RATIO = 3;
+
+  // for each item in array, record each char used and number of characters
+  // compare each item's characters and character totals; if they are even, they are anagrams and should be grouped together.
+  // 
+
+  for (let i = 0; i < arr.length; i++) {
+    let item = 0;
+    for (let j = 0; j < arr[i].length; j++) {
+      item += arr[i].charCodeAt(j);
+    }
+    arrMap.set(arr[i], item);
+  }
+
+  let finalArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let itemCode = arrMap.get(arr[i]);
+    let groupArr = [];
+    for (let j = 0; j < arr.length; j++) {
+      if (itemCode === arrMap.get(arr[j])) {
+        groupArr.push(arr[j]);
+        arrMap.delete(arr[j]);
+        console.log(groupArr);
+        console.log(arrMap);
+      }
+    }
+    arr.filter((item, i) => {
+      console.log(groupArr[i]);
+      console.log(item);
+      groupArr[i] !== item;
+    });
+    console.log(arr);
+    finalArr.push(groupArr);
+  }
+  return finalArr;
+}
+
+
+
+const testArr = ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race'];
+const outcome = [['east', 'teas', 'eats'], ['cars', 'arcs'], ['acre', 'race']];
+
+console.log('Testing outcome of anagramGrouping()...');
+console.log('');
+console.log(anagramGrouping(testArr));
+console.log('');
+console.log('----------');
+
+if (anagramGrouping(testArr) === outcome) {
+  console.log('Ace!');
+} else {
+  console.log('Bummer...');
+}
