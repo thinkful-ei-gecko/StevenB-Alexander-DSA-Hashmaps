bde84ee5bda023f8ec97f472e7081974d06c48d5
Steven Bull <stevenwbulljr@gmail.com>
2019-11-11 17:36:23 +0000

    finish question 1

---
+++ Answers.md
@@ -0,0 +1,5 @@
+1. Once retrieving the 'Hobbit' and 'Maiar' keys we notice the discrepancy that he are missing one value for each key. For Hobbit we should receive Frodo and Bilbo, but we receive only Frodo, and with Maiar we should receive The Necromancer and Sauron but only receive Sauron.
+
+1.2 Once our HashMap is ran it increases the capacity from the initial value of 8 to 24 in order to make room for additional key value pairs. This is done because our Max Load ratio of 50% was surpassed.
+
+2. 
--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -1,5 +1,26 @@
 const HashMap = require('./HashMap');
 
 const main = () => {
+  let lor = new HashMap();
 
+  HashMap.MAX_LOAD_RATIO = 0.5;
+  HashMap.SIZE_RATIO = 3;
+
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandalf');
+  lor.set('Human', 'Aragorn');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+  
+  // return lor.get('Hobbit');
+  // return lor.get('Maiar');
+  return lor;
 };
+
+console.log(main());
